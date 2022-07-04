198f86bd3688a96d535219f6d2c7f534be2bea58
GuruSaiKumar <gurusaikumar2002@gmail.com>
2022-07-04 10:02:50 +0530

    Removved uneccesary imports

--- cronjob/src/main/java/com/sprinklr/Cronjob/CronjobApplication.java
+++ cronjob/src/main/java/com/sprinklr/Cronjob/CronjobApplication.java
@@ -2,10 +2,8 @@ package com.sprinklr.Cronjob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableScheduling
--- cronjob/src/main/java/com/sprinklr/Cronjob/config/ConfigCronjob.java
+++ cronjob/src/main/java/com/sprinklr/Cronjob/config/ConfigCronjob.java
@@ -3,7 +3,6 @@ package com.sprinklr.Cronjob.config;
 import com.sprinklr.JStack.Analyser.config.ConfigApi;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 
 //This is required to import the beans from Our API
 @Configuration
